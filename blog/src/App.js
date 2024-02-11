e49aa03509d15fdfd4e198a9d27083ced6f7ab7d
SeoMiYoung <seo20seo00@naver.com>
2024-02-12 00:17:33 +0900

    변수를 html에 꽂아넣을 땐 중괄호 사용하기

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let post = '강남 우동 맛집';
+
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>Miyoung's Blog</h4>
       </div>
+      <h4>{post}</h4>
     </div>
   );
 }
