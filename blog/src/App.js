b2f78ac83d1e16604a554ac250b117abb5e45a29
SeoMiYoung <seo20seo00@naver.com>
2024-02-13 17:23:14 +0900

    state, onClick을 이용해서 버튼을 누르면 글제목을 가나다순으로 정렬하는 기능 만들기

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,64 @@
+// Lint를 끄는 기능임
+/* eslint-disable */ 
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  let post = '강남 우동 맛집';
+  // a 작명: state에 보관했던 자료가 나옴
+  // b 작명: state 변경을 도와주는 함수
+  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [good, setGood] = useState([0,0,0]);
+
+  // onClick
+  function clickGood() {
+    // setGood(good[0]+1);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color: 'pink', fontSize: '20px'}}>Miyoung's Blog</h4>
+        <h4>React Blog</h4>
       </div>
-      <h4>{post}</h4>
+      <button onClick={()=>{
+        let copy = [...title];
+        copy.sort(); // 가나다순 정렬
+        setTitle(copy);
+      }}>
+        가나다순 정렬
+      </button>
+      <button onClick={()=>{
+        // 일부만 바꿔서 state 변경 함수에 넣기(확장성 있는 코드)
+        // 화살표를 새로 바꿔주세요~
+        let copy = [...title];
+        copy[0] = '여성 코트 추천';
+        setTitle(copy);
+      }}>성별 바꾸기</button>
+      <div className="list">
+        <h4>
+          {title[0]}
+          <span>👍</span>
+          0
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>
+          {title[1]}
+          <span>👍</span>
+          0
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>
+          {title[2]}
+          <span onClick={clickGood}>👍</span>
+          0
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
