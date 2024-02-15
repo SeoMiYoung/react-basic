c4b5331227b812ff519fbaf56c20f8754c635162
SeoMiYoung <seo20seo00@naver.com>
2024-02-15 18:38:27 +0900

    쇼핑몰 대문 만들기 & css파일에서 이미지 넣기

--- shop/src/App.js
+++ shop/src/App.js
@@ -9,14 +9,17 @@ function App() {
       {/* 참고로 Bootstrap에서 가져온것도 컴포넌트에 className을 붙혀서 추가로 커스텀할 수 있음 */}
       <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">Ming Shop</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#menu">Menu</Nav.Link>
+          <Nav.Link href="#cart">Cart</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg">
+      </div>
     </div>
   );
 }
