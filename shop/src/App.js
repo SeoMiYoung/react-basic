7b5f76ea0b7da557b9cd765e8c8778324d07746e
SeoMiYoung <seo20seo00@naver.com>
2024-02-16 02:46:48 +0900

    [myself] 오늘의 숙제: nested routes써서 페이지 만들기

--- shop/src/App.js
+++ shop/src/App.js
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="App">
       {/* 참고로 Bootstrap에서 가져온것도 컴포넌트에 className을 붙혀서 추가로 커스텀할 수 있음 */}
+      {/* 아래 Navbar도 공용 컴포넌트로 빼낼 수 있을 듯. 근데 귀찮아서 안함. */}
       <Navbar bg="dark" variant="dark">
         <Container>
         <Navbar.Brand href="#home">Ming's Shoe Shop</Navbar.Brand>
@@ -29,9 +30,22 @@ function App() {
         <Route path="/" element={<Home shoes={shoes}/>} />
         <Route path="/detail" element={<Detail/>} />
         <Route path="*" element={<div>없는 페이지 입니다.</div>} /> 
+        <Route path="/event" element={<Event/>}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
       </Routes>
     </div>
   );
+}
+
+function Event() {
+  return (
+    <>
+      <div>오늘의 이벤트</div>
+      <Outlet></Outlet>
+    </>
+  )
 }
 
 export default App;
