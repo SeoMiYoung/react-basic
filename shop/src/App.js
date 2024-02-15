334175707f18da67b5409e375efaeb822b37b2a1
SeoMiYoung <seo20seo00@naver.com>
2024-02-15 20:26:51 +0900

    상품명과 가격 데이터 바인딩

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,12 @@
-
+import { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import bg from './img/bg.png'
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import bg from './img/bg.png';
+import data from './data.js';
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
       {/* 참고로 Bootstrap에서 가져온것도 컴포넌트에 className을 붙혀서 추가로 커스텀할 수 있음 */}
@@ -27,18 +29,18 @@ function App() {
         <div className="row">
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </div>
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </div>
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
           </div>
         </div>
       </div>
