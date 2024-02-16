79e43d27eb8f247157822bb838f6c186dc621e17
SeoMiYoung <seo20seo00@naver.com>
2024-02-16 22:58:34 +0900

    styled-components 라는 인기 라이브러리를 사용하여 이쁜 스타일을 입힌 <button>만들기

--- shop/src/page/detail/Detail.js
+++ shop/src/page/detail/Detail.js
@@ -1,6 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+let YellowBtn = styled.button`
+    background : yellow;
+    color : black;
+    padding : 10px;
+`
 
 function Detail(props) {
     let { id } = useParams(); // 현재 url의 파라미터 정보들이 남음(모르겠으면 구글링 ㄱㄱ)
@@ -9,6 +16,7 @@ function Detail(props) {
 
     return (
         <div className="container">
+            <YellowBtn>버튼</YellowBtn>
             <div className="row">
                 <div className="col-md-6">
                     <img src={"https://codingapple1.github.io/shop/shoes" + imgNum + ".jpg"} width="100%" />
