33065b18d993cd20181df4bbed072c4f3759886a
SeoMiYoung <seo20seo00@naver.com>
2024-02-17 22:07:02 +0900

    useSelector를 잘 써서 특정 state만 가져와서 쓸 수 있음

--- shop/src/page/cart/Cart.js
+++ shop/src/page/cart/Cart.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 function Cart() {
     // getState에는 Redux store에 있던 state가 남습니다
     let getState = useSelector((state)=>{
-        return state
+        return state.user
     }) // Redux store 가져와줌
     console.log(getState.user);
     console.log(getState.stock);
