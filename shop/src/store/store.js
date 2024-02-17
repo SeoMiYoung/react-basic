6a7f7cfb829c859963c5da94da83ad2ae2da5084
SeoMiYoung <seo20seo00@naver.com>
2024-02-17 21:53:32 +0900

    user라는 slice를 등록하기

--- shop/src/store/store.js
+++ shop/src/store/store.js
@@ -1,5 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+// state 하나를 slice라고 함
+let user = createSlice({ // useState랑 비슷한 용도
+    name : 'user',
+    initialState : 'ming'
+})
 
 export default configureStore({
-  reducer: { }
+  reducer: { // 등록하기
+    user : user.reducer // 작명 : state명.reducer
+  }
 }) 
