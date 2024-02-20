4ef4a54033c40e176dd726d50b14659771828260
SeoMiYoung <seo20seo00@naver.com>
2024-02-20 18:06:21 +0900

    useQuery로 감싸서 ajax요청을 하자(react-query)

--- shop/src/App.js
+++ shop/src/App.js
@@ -8,6 +8,7 @@ import Home from './page/home/Home.js';
 import Cart from './page/cart/Cart.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import axios from 'axios';
+import { useQuery } from "react-query";
 
 export let Context1 = createContext(); // context를 하나 만들어줌(state 보관함)
 
@@ -21,6 +22,14 @@ function App() {
   let [shoes, setShoes] = useState(data);
   let navigate = useNavigate();
 
+  // react-query를 이용해서 ajax 요청을 해보자
+  useQuery('작명', ()=>{
+    axios.get('https://codingapple1.github.io/userdata.json')
+      .then((a)=>{
+        a.data
+      })
+  })
+
   return (
     <div className="App">
       {/* 참고로 Bootstrap에서 가져온것도 컴포넌트에 className을 붙혀서 추가로 커스텀할 수 있음 */}
@@ -32,6 +41,7 @@ function App() {
           <Nav.Link onClick={()=>{ navigate('/') }}>Home</Nav.Link>
           <Nav.Link onClick={()=>{ navigate('/cart') }}>Cart</Nav.Link>
         </Nav>
+        <Nav className="ms-auto">반가워요 Seo</Nav>
         </Container>
       </Navbar>
 
