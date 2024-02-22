4f6bc2026eb8922232b30280b8177ccdd2321def
SeoMiYoung <seo20seo00@naver.com>
2024-02-22 14:10:28 +0900

    node서버와 테스트 리액트 페이지 연결

--- makeNodeServer/server.js
+++ makeNodeServer/server.js
@@ -4,4 +4,12 @@ const app = express();
 
 app.listen(8080, function () {
   console.log('listening on 8080')
-}); 
+});
+
+// [1] 해당 경로에 있는 static 파일들을 가져다가 쓰겠다고 선언
+app.use(express.static(path.join(__dirname, 'test-web-page\\build')));
+
+// [2] 메인 페이지로 접속 시, '리액트로 만든 html 파일 경로'보여주세요~ 라고 하는거임
+app.get('/', function(요청, 응답) {
+  응답.sendFile(path.join(__dirname, 'test-web-page\\build\\index.html'));
+});
