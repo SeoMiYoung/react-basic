8f70c0161431c779f11424a3685fd86a31edf209
SeoMiYoung <seo20seo00@naver.com>
2024-02-22 13:41:30 +0900

    간단하게 페이지 텍스트 변경

--- makeNodeServer/test-web-page/src/App.js
+++ makeNodeServer/test-web-page/src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          서버 연결 테스트용 웹페이지 입니다.
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          연결을 성공시켜보자~
         </a>
       </header>
     </div>
